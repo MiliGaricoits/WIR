0e987318ccbc31f43f903e7962db7c40b0b5a212
im4184293 <im4184293@926869Lx.(none)>
2014-11-05 14:16:02 -0200

    avances

--- js/mainScreen.js
+++ js/mainScreen.js
@@ -2,45 +2,63 @@
 
 $(document).ready(function(){
 
-	FB.init({//la app tiene que tener una url
-		appId      : 'id-app',
-	    status     : true,
-	    xfbml      : true,
-	    version    : 'v2.0'
-	});
+	$('#fbLoginButton').click(function(){
 
-	FB.getLoginStatus(function(response){
+		FB.getLoginStatus(function(response){
 
-		if (response.status === 'connected') {
+			if (response.status === 'connected') {
 
-			// the user is logged in and has authenticated your
-			// app, and response.authResponse supplies
-			// the user's ID, a valid access token, a signed
-			// request, and the time the access token 
-			// and signed request each expire
+				// the user is logged in and has authenticated your
+				// app, and response.authResponse supplies
+				// the user's ID, a valid access token, a signed
+				// request, and the time the access token 
 
-		} 
-		else if (response.status === 'not_authorized'){
+				FB.api('/me/movies', function(response) {
 
-			// the user is logged in to Facebook, 
-			// but has not authenticated your app
+					var container = $('center');
 
-		}
-		else{
+					container.append('</br>');
+					container.append('</br>');
 
-			// the user isn't logged in to Facebook.
-			
-			FB.login(function(){
+					for (var i = 0; i<response.data.length; i++){
+						container.append('</br>');
+						container.append(response.data[i].name);
+					};
 
-				FB.api('/me/likes', function(response) {
-
-					//en response vienen los likes de guacho
+					container.append('</br>');
+					console.log(response);
 
 				});
 
-			});
+			} 
+			else{
 
-		};
+				// the user isn't logged in to Facebook.
+				
+				FB.login(function(){
+
+					FB.api('/me/movies', function(response) {
+
+						var container = $('center');
+
+						container.append('</br>');
+						container.append('</br>');
+
+						for (var i = 0; i<response.data.length; i++){
+							container.append('</br>');
+							container.append(response.data[i].name);
+						};
+
+						container.append('</br>');
+						console.log(response);
+
+					});
+
+				}, {scope: 'user_likes'});
+
+			};
+
+		});
 
 	});
 
