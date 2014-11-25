10c33b9cc1a7bf5f05d5df0cce0ce69b01e92676
MiliGaricoits <mili.garicoits@gmail.com>
2014-11-25 21:44:17 -0200

    recomendados, carrusel

--- js/recommended.js
+++ js/recommended.js
@@ -1,4 +1,3 @@
-
 var pelis = [
 	{
 		id: "0", 
@@ -7,7 +6,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 3,
-		like: true	
+		like: 0	
 	},
 	{
 		id: "1", 
@@ -16,7 +15,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 3,
-		like: true	
+		like: 0	
 	},
 	{
 		id: "2", 
@@ -25,7 +24,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: -1,
-		like: false	
+		like: -1	
 	},
 	{
 		id: "3", 
@@ -34,7 +33,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 1,
-		like: true	
+		like: 0	
 	},
 	{
 		id: "1", 
@@ -43,7 +42,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 3,
-		like: true	
+		like: 0	
 	},
 	{
 		id: "2", 
@@ -52,7 +51,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: -1,
-		like: false	
+		like: -1	
 	},
 	{
 		id: "3", 
@@ -61,7 +60,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 1,
-		like: true	
+		like: 0	
 	},
 	{
 		id: "1", 
@@ -70,7 +69,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 3,
-		like: true	
+		like: 0	
 	},
 	{
 		id: "2", 
@@ -79,7 +78,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: -1,
-		like: false	
+		like: -1	
 	},
 	{
 		id: "3", 
@@ -88,7 +87,7 @@ var pelis = [
 		description_short: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
 		description_long: "Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.",
 		rating: 1,
-		like: true	
+		like: 0	
 	}
 ];
 
@@ -99,14 +98,14 @@ $(document).ready(function(){
 	globales.setServer("localhost:15000/");
 	var server = globales.getServer();
 
-	//_cargarContenidoPeliculas(pelis);
+	_cargarRecomendados(pelis);
 
 	$("#nav_home").attr("class", '');
 	$("#nav_top10").attr("class", '');
 	$("#nav_recommended").attr("class", 'active');
 	$("#nav_my_movies").attr("class", '');
 	
-	$.ajax({
+	/*$.ajax({
         type : "GET",
         url: "http://"+server+"getPeliculas?idFacebook=1162593356",//+globales.getFacebookID(),
         contentType: 'application/x-www-form-urlencoded',
@@ -119,105 +118,22 @@ $(document).ready(function(){
     .fail(function(jqXHR, textStatus) {
 
     	alert(jqXHR.responseText);
-	});
+	});*/
 
 });
 
-function _cargarContenidoPeliculas(peliculas)
+function _cargarRecomendados(recomendados)
 {
-	var peli_ppl = $('<div class="col-lg-4 img_peli_principal">');
-	var img_ppl = $('<div id="img_ppl"></div>').css('background-image', 'url('+ pelis[0].poster +')');
-	var rating_ppl = $('<input type="hidden" class="rating" readonly data-min="1" data-max="5"/>').attr('value', peliculas[0].likesPelicula);
-	rating_ppl.rating({
-		filled: 'glyphicon-star',
-		empty: 'glyphicon-star-empty',
-		start: 0,
-		stop: 10,
-		step: 1
-	});
-	var like_ppl = $('<input type="hidden" class="rating" readonly data-filled="glyphicon glyphicon-heart" data-empty="glyphicon glyphicon-heart-empty" data-stop="1"/>').attr('value', pelis[0].like);
-	//var long_desc_ppl = $('<p id="desc_ppl"></p>').text(peliculas[0].descripcionPelicula);
-	var titulo_ppl = $('<h3 id="titulo_ppl"></h3>').text(peliculas[0].titulo);
-	var long_desc_ppl = $('<p id="desc_ppl"></p>').text(peliculas[0].description_long);
-
-	img_ppl.appendTo(peli_ppl);
-	rating_ppl.appendTo(peli_ppl);
-	like_ppl.appendTo(peli_ppl);
-	titulo_ppl.appendTo(peli_ppl);
-	long_desc_ppl.appendTo(peli_ppl);
-	peli_ppl.appendTo($("#movies_list"));
-
 	var index = 0;
-	peliculas.forEach(function(peli){
-		var peli_small = $('<div id="' + index + '" class="col-lg-2 img_peli">');
-		//var img_small = $('<img id="img_ppl">').attr('src', "../assets/movies/movie" + index%3+1 + ".png");
-		var img_small = $('<img id="img_ppl">').attr('src', peli.poster);
-		var peli_span = $('<span class="caption scale-caption"></span>');
-		var peli_title = $('<h3></h3>').text('Titulo');
-		//var peli_desc_short = $('<p></p>').text(peli.descripcionPelicula);
-		var peli_desc_short = $('<p></p>').text(peli.description_short);
-	
-		img_small.appendTo(peli_small);
-		peli_title.appendTo(peli_span);
-		peli_desc_short.appendTo(peli_span);
-		peli_span.appendTo(peli_small);
-		peli_small.appendTo($("#movies_list"));
+	recomendados.forEach(function (peli) {
+
+		var new_item = '<div id="' + index + '" class="div_recommended">';
+		new_item += '<img id="img_recommended" src="' + peli.poster + '">';
+		new_item += '</div>';
+
+		$('#effects').sly('add', '<li>' + new_item + '</li>');
+		$('#effects').sly('toCenter', 3);
 
 		index++;
-
 	});
-
-	$(".col-lg-2").click(function(){
-		var peli_id = $(this).attr('id');
-		$('#img_ppl').css('background-image', 'url('+ pelis[peli_id].poster +')');
-		$('#titulo_ppl').text(pelis[peli_id].titulo);
-		$('#desc_ppl').text(pelis[peli_id].description_long);
-	})
 }
-
-/*function mapDOM(element, json) {
-    var treeObject = {};
-
-    // If string convert to document Node
-    if (typeof element === "string") {
-        if (window.DOMParser) {
-              parser = new DOMParser();
-              docNode = parser.parseFromString(element,"text/xml");
-        } else { // Microsoft strikes again
-              docNode = new ActiveXObject("Microsoft.XMLDOM");
-              docNode.async = false;
-              docNode.loadXML(element); 
-        } 
-        element = docNode.firstChild;
-    }
-
-    //Recursively loop through DOM elements and assign properties to object
-    function treeHTML(element, object) {
-        object["type"] = element.nodeName;
-        var nodeList = element.childNodes;
-        if (nodeList != null) {
-            if (nodeList.length) {
-                object["content"] = [];
-                for (var i = 0; i < nodeList.length; i++) {
-                    if (nodeList[i].nodeType == 3) {
-                        object["content"].push(nodeList[i].nodeValue);
-                    } else {
-                        object["content"].push({});
-                        treeHTML(nodeList[i], object["content"][object["content"].length -1]);
-                    }
-                }
-            }
-        }
-        if (element.attributes != null) {
-            if (element.attributes.length) {
-                object["attributes"] = {};
-                for (var i = 0; i < element.attributes.length; i++) {
-                    object["attributes"][element.attributes[i].nodeName] = element.attributes[i].nodeValue;
-                }
-            }
-        }
-    }
-    treeHTML(element, treeObject);
-
-    return (json) ? JSON.stringify(treeObject) : treeObject;
-}*/
